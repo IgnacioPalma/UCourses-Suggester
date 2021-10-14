a1a8aa191d97aef7c8a74d72c7b5924fcfc2f7d2
Bairond-code <ignacio.palma@uc.cl>
2021-10-14 16:55:12 -0300

    Solved bugs and deleted copy-files

--- courses.py
+++ courses.py
@@ -1,28 +1,27 @@
 import pandas as pd
 
-class Request():
+class Properties():
 
-    def __init__(self, course_name):
-        """ Define and assign the properties of the course """
+    def __init__(self, name):
+        """ Define a course with his name and properties """
 
         # Extract information from "Catalogo UC" into data frames
-        data_frames = pd.read_html("http://catalogo.uc.cl/index.php?tmpl=component&option=com_catalogo&view=requisitos&sigla=MAT1620")
+        data_frames = pd.read_html(f"http://catalogo.uc.cl/index.php?tmpl=component&option=com_catalogo&view=requisitos&sigla={name}")
         first_table = data_frames[0].to_dict(orient='records') # Convert the first table into a dictionary
         
         # Initialization of dictionary
         properties = {}
  
-        # Loop through every row in the first table
+        # Loop through every row in the first table given by "Catalogo UC"
         for row in first_table:
 
-            # Extract the row values and convert into a list
+            # Convert row values into a list
             items = list(row.values())
 
             # Assign values in dictionary of properties
             properties[items[0]] = items[1]
 
         # Assignment
-        self.course_name = course_name
         self.properties = properties
 
     def prerequisites(self):
