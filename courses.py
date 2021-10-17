0a5e428c8a4300f5a9c98b99f5232ab0cfa74d48
Bairond-code <ignacio.palma@uc.cl>
2021-10-17 18:37:13 -0300

    Indev request class for optimization

--- courses.py
+++ courses.py
@@ -1,29 +1,41 @@
 import pandas as pd
+import concurrent.futures
+
+def Request(course):
+    """ Request and return the properties of a given course """
+ 
+    # Extract information from "Catalogo UC" into data frames
+    data_frames = pd.read_html(f"http://catalogo.uc.cl/index.php?tmpl=component&option=com_catalogo&view=requisitos&sigla={course}")
+    first_table = data_frames[0].to_dict(orient='records') # Convert the first table into a dictionary
+
+    # Initialization of dictionary
+    properties = {}
+
+    # Loop through every row in the first table given by "Catalogo UC"
+    for row in first_table:
+
+        # Convert row values into a list
+        items = list(row.values())
+
+        # Assign values in dictionary of properties
+        properties[items[0]] = items[1]
+    
+    print(properties)
+    # Return statement
+    return properties
+
+MAX_THREADS = 30
 
 class Properties():
 
-    def __init__(self, name):
-        """ Define a course with his name and properties """
+    def __init__(self, course):
+        """ Define a course with his properties """
 
-        # Extract information from "Catalogo UC" into data frames
-        data_frames = pd.read_html(f"http://catalogo.uc.cl/index.php?tmpl=component&option=com_catalogo&view=requisitos&sigla={name}")
-        first_table = data_frames[0].to_dict(orient='records') # Convert the first table into a dictionary
-        
-        # Initialization of dictionary
-        properties = {}
- 
-        # Loop through every row in the first table given by "Catalogo UC"
-        for row in first_table:
+        with concurrent.futures.ThreadPoolExecutor(3) as executor:
+            properties = executor.submit(Request, course)
 
-            # Convert row values into a list
-            items = list(row.values())
-
-            # Assign values in dictionary of properties
-            properties[items[0]] = items[1]
-
-        # Assignment
         self.properties = properties
-
+        
     def prerequisites(self):
         """ Return the values of the prerequisites of the course """
         return self.properties["Prerrequisitos"]
--- test.py
+++ test.py
@@ -1,10 +1,17 @@
-import alumni, pandas
+from courses import Properties
+from recommend import Alumni
+import time
+startTime = time.time()
 
 """INFORMATION PROVIDED BY USER"""
 # Degree given by the user
 degree = "Ingeneria Comercial"
 
 # Courses already taken by the user
-courses = ["MAT1620", "EAA1510"]
+courses = ["MAT1620"]
 
 """PROGRAM"""
+Properties("MAT1630")
+
+executionTime = (time.time() - startTime)
+print('Execution time in seconds: ' + str(executionTime))
